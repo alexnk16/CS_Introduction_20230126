5380c6d87809c7551f5ad59d43256e95b466b954
Alexander <alexander_nk@mail.ru>
2023-02-01 16:13:26 +0300

    Lesson1 01

---
+++ 002/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double a,b;
+string s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+
+if (a*a==b)
+    //System.Console.WriteLine("b является квадратом a");
+    System.Console.WriteLine("b является квадратом a");
+if (b*b==a)
+System.Console.WriteLine("a является квадратом b");
