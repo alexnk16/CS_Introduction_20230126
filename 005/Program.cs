394e1e19c9e0995efc9e0c61315a0b01f5a7fd42
Alexander <alexander_nk@mail.ru>
2023-02-02 15:30:02 +0300

    Lesson1 05 error

---
+++ 005/Program.cs
@@ -0,0 +1,33 @@
+﻿// С клавиатуры вводятся два числа. Найти и вывести на экран максимальное из них
+int a,b,c;
+System.Console.Write("Введите a:");
+a=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите b:");
+b=Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите c:");
+c=Convert.ToInt32(Console.ReadLine());
+
+
+int max;
+
+
+if (a>b)
+{
+max=a;
+}
+else
+{
+    max=b;
+}
+
+if (a>c)
+{
+max=a;
+}
+else
+{
+    max=c;
+}
+
+System.Console.WriteLine($"max={max}");
+
