454fc2dc213f8f1a325ac96af096d9b70727d42f
Alexander <alexander_nk@mail.ru>
2023-02-15 16:26:49 +0300

    lesson 5

--- 005/Program.cs
+++ 005/Program.cs
@@ -7,10 +7,8 @@ b=Convert.ToInt32(Console.ReadLine());
 System.Console.Write("Введите c:");
 c=Convert.ToInt32(Console.ReadLine());
 
-
 int max =a;
 if (b>max) max=b;
 if (c>max) max=c;
 
 System.Console.WriteLine($"max={max}");
-
